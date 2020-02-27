18f5923d2f0bc2b0bb37dcc7c4f6b0e59b768720
Skyler Tosh <skylerjtosh@gmail.com>
2020-02-27 14:14:45 -0800

    Updating index.js

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-var inquirer = require('inquirer');
-var fs = require('fs');
+var inquirer = require("inquirer");
+var fs = require("fs");
 var axios = require("axios");
-var generateHTML = require('./generateHTML.js');
-const questions =
-    [
-        {
-            type: 'input',
-            message: 'What is your favorite color?',
-            name: 'color'
-        },
-        {
-            type: 'input',
-            message: 'Enter your GitHub username',
-            name: 'username'
-        }
-	];
-	
+var generateHTML = require("./generateHTML.js");
+const questions = [
+  {
+    type: "input",
+    message: "What is your favorite color?",
+    name: "color"
+  },
+  {
+    type: "input",
+    message: "Enter your GitHub username",
+    name: "username"
+  }
+];
 
-	function init() {
-		inquirer.prompt(questions)
-	}
+function fetchGithubData(username) {
+}
 
-	init();
+function writeToFile(fileName, data) {
+ 
+}
+
+function init() {
+  inquirer.prompt(questions).then(function(responses) {
+    fetchGithubData(responses.username);
+  });
+}
+
+init();
