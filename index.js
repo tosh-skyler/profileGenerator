e76d1fe7a1d5c923d51e275ae12f7632e5eda131
Skyler Tosh <skylerjtosh@gmail.com>
2020-02-27 13:35:09 -0800

    Adding inquirer to prompt user input

--- index.js
+++ index.js
@@ -2,3 +2,23 @@ var inquirer = require('inquirer');
 var fs = require('fs');
 var axios = require("axios");
 var generateHTML = require('./generateHTML.js');
+const questions =
+    [
+        {
+            type: 'input',
+            message: 'What is your favorite color?',
+            name: 'color'
+        },
+        {
+            type: 'input',
+            message: 'Enter your GitHub username',
+            name: 'username'
+        }
+	];
+	
+
+	function init() {
+		inquirer.prompt(questions)
+	}
+
+	init();
